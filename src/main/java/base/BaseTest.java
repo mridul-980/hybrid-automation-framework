3a278a7be7e83c1c0407c51c51596ca880a31806
Mridul <mridultripathi204@gmail.com>
2026-07-02 10:41:26 +0530

    Implement parallel cross-browser execution using Selenium Grid

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -2,6 +2,9 @@ package base;
 
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+
 import drivers.BrowserManager;
 import factory.DriverFactory;
 import utilities.ConfigReader;
@@ -10,12 +13,14 @@ import utilities.RuntimeConfig;
 public class BaseTest {
 
 	@BeforeMethod
-	public void setup() {
+	@Parameters("browser")
+	public void setup(
+	        @Optional("chrome") String browser) {
 
 	    Log.info("Browser setup started");
 
 	    BrowserManager.initializeBrowser(
-	            RuntimeConfig.getBrowser(),
+	            browser,
 	            RuntimeConfig.isHeadless());
 
 	    DriverFactory.getDriver()
